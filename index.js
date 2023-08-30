487ceacbcd6cfb5830e45434cab1bc14e23f6eca
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-08-30 15:49:46 +0100

    Completed assignments

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+      // split the original string into an array of letters
+  const lettersArray = word.split('');
+    
+      // create a reverse string-array of the original word
+  const reverseArray = [];
+  for (let i = 0; i < lettersArray.length; i++) {
+      reverseArray.unshift(lettersArray[i]);
+  }
+  
+      // convert the string-array into a string
+  const reverseWord = reverseArray.join('');
+  
+      // check if the original string matches the new string
+  return word === reverseWord;
 }
 
 /* 
-  Add your pseudocode here
+  1- reverse the input string
+  
+  2- check ( reverse string VS input string )
+
+  3- return test-result boolean value
+
 */
 
 /*
-  Add written explanation of your solution here
+
+  i need to write a function that takes a string as input then reverse it's letter ordering,the function check if the generated string is the same as the string input.
+  if yes the function return a positive boolean and vice versa. 
+
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wow"));
+
+  console.log("");
+
+  console.log("Expecting: false"); // to test for case sensitvity
+  console.log("=>", isPalindrome("Wow"));
+
+  console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("MOM"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("444"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("4554"));
+  
   console.log("");
 
   console.log("Expecting: false");
